b193504dbabeb4cda27ee5981523c900064cdbc1
Slimgaga <atunushabest@gmail.com>
2022-09-08 16:37:01 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,15 @@
-#include <stdio.h>
-
-/**
- *  * Description: main - Prints out a string to stdout.
- *   * Return: 0 if success.
- *    */
-
-int main(void)
-
-{
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
-}
+#include <stdio.h> 
+  
+ /** 
+  *   * main - Entry point 
+  *    * 
+  *     * Description: A C program that prints with put function 
+  *      * 
+  *       * Return: Always 0 (Success) 
+  *       */ 
+  
+ int main(void) 
+	 { 
+		          puts("\"Programming is like building a multilingual puzzle"); 
+		           return (0); 
+		    }
